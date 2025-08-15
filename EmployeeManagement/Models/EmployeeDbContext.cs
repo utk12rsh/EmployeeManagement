e5c0bbacde7a3f43ed3d591d4c8c7d310c64af1c
Utkarsh Tripathi <133485209+utk12rsh@users.noreply.github.com>
2025-08-15 18:57:57 +0530

    Update EmployeeDbContext.cs

--- EmployeeManagement/Models/EmployeeDbContext.cs
+++ EmployeeManagement/Models/EmployeeDbContext.cs
@@ -21,7 +21,7 @@ public partial class EmployeeDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=UTKARSHTRIPATHI;Database=EmployeeDB;Integrated Security=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=YOUR_DATABASE_CONNECTION_STRING_HERE");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
